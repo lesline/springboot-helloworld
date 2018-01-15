148907858bdf8cd8edee8df286ff89127cc0de94
zhangshaolin <zhangshaolin@jd.com>
2018-01-15 19:34:07 +0800

    Initial commit

---
+++ src/main/java/springboot/Application.java
@@ -0,0 +1,17 @@
+package springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot应用启动类
+ *
+ * Created by bysocket on 16/4/26.
+ */
+@SpringBootApplication
+public class Application {
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
